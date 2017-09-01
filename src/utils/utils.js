63b45def2dfe75effaf3fcd31cb5c0fce423e5bf
冯冬冬 <fengdd@hdsxtech.com>
2017-09-01 14:34:59 +0800

    # add popover 100%

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -34,7 +34,23 @@ const checkBrowser = () => {
   }
 }
 
+/**
+ * 获取uuid
+ * @returns {*|string|!Array.<T>}
+ */
+const getuuid = () => {
+  let [ s, hexDigits ] = [ [], '0123456789abcdef' ]
+  for (let i = 0; i < 36; i++) {
+    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
+  }
+  s[14] = '4'
+  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1)
+  s[8] = s[13] = s[18] = s[23] = '-'
+  return (s.join(''))
+}
+
 export {
   has,
-  checkBrowser
+  checkBrowser,
+  getuuid
 }
