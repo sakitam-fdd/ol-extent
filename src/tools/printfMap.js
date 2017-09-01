309e83fc4f01a49e1ae658d6489d5ae0cf7ce709
冯冬冬 <fengdd@hdsxtech.com>
2017-09-01 11:07:34 +0800

    # add printf tools 70%

--- src/tools/printfMap.js
+++ src/tools/printfMap.js
@@ -2,8 +2,10 @@
  * Created by FDD on 2017/8/31.
  * @ 地图打印功能
  */
-import has from 'lodash/has'
 import 'core-js/es6/promise'
+import {has} from '../utils/utils'
+import fileSaver from 'file-saver'
+import JSPDF from 'jspdf'
 if (!has(ol, 'tools')) {
   ol.tools = {}
 }
@@ -14,6 +16,32 @@ ol.tools.PrintfMap = function (map, params) {
     this.map = map
   } else {
     throw new Error('未获取到地图对象！')
+  }
+
+  /**
+   * 生成pdf大小
+   * @type {{a0: [*], a1: [*], a2: [*], a3: [*], a4: [*], a5: [*]}}
+   */
+  this.dims = {
+    a0: [1189, 841],
+    a1: [841, 594],
+    a2: [594, 420],
+    a3: [420, 297],
+    a4: [297, 210],
+    a5: [210, 148]
+  }
+
+  /**
+   * 纸张类型
+   * @type {{A0: string, A1: string, A2: string, A3: string, A4: string, A5: string}}
+   */
+  this.format = {
+    A0: 'a0',
+    A1: 'a1',
+    A2: 'a2',
+    A3: 'a3',
+    A4: 'a4',
+    A5: 'a5'
   }
 }
 
@@ -44,7 +72,7 @@ ol.tools.PrintfMap.prototype.saveCanvas = function (event, params) {
  * @param params
  * @returns {Promise}
  */
-ol.tools.PrintfMap.prototype.getImage = function (params) {
+ol.tools.PrintfMap.prototype.getImageUrl = function (params) {
   this.map.renderSync()
   if (params['printfType'] === 'jpg') {
     params['printfType'] = 'jpeg'
@@ -58,8 +86,57 @@ ol.tools.PrintfMap.prototype.getImage = function (params) {
   })
 }
 
-ol.tools.PrintfMap.prototype.getPDF = function () {
+/**
+ * 下载图片
+ * @param params
+ * @constructor
+ */
+ol.tools.PrintfMap.prototype.DownLoadImage = function (params) {
+  let button = document.createElement('a')
+  button.style.visibility = 'hidden'
+  button.style.width = '0px'
+  button.style.height = '0px'
+  document.body.appendChild(button)
+  button.addEventListener('click', () => {
+    this.map.once('postcompose', event => {
+      let canvas = event.context.canvas
+      if (navigator.msSaveBlob) {
+        navigator.msSaveBlob(canvas.msToBlob(), (params['fileName'] ? params['fileName'] : 'map') + '.png')
+      } else {
+        canvas.toBlob(function (blob) {
+          fileSaver.saveAs(blob, (params['fileName'] ? params['fileName'] : 'map') + '.png')
+        })
+      }
+    })
+    this.map.renderSync()
+  })
+  button.click()
+}
 
+/**
+ * 下载为pdf
+ * @param params
+ * @constructor
+ */
+ol.tools.PrintfMap.prototype.DownLoadPDF = function (params) {
+  let button = document.createElement('a')
+  button.style.visibility = 'hidden'
+  button.style.width = '0px'
+  button.style.height = '0px'
+  document.body.appendChild(button)
+  button.addEventListener('click', () => {
+    this.getImageUrl(params).then(res => {
+      let format = this.format[params['format']] ? this.format[params['format']] : 'a4'
+      let dim = this.dims[format]
+      let pdf = new JSPDF(params['orient'] ? params['orient'] : 'landscape', undefined, format)
+      pdf.addImage(res, 'JPEG', 0, 0, dim[0], dim[1])
+      pdf.save((params['fileName'] ? params['fileName'] : 'map') + '.pdf')
+    }).catch(error => {
+      console.log(error)
+    })
+    this.map.renderSync()
+  })
+  button.click()
 }
 
 ol.tools.PrintfMap.prototype.print = function () {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,3 +3,38 @@
  * @ 工具类
  */
 
+/**
+ * 判断对象是否有某个键值
+ * @param object_
+ * @param key_
+ * @returns {boolean}
+ */
+const has = (object_, key_) => {
+  return typeof object_ === 'object' && object_.hasOwnProperty(key_)
+}
+
+/**
+ * 检查浏览器版本
+ * @returns {*}
+ */
+const checkBrowser = () => {
+  let userAgent = navigator.userAgent // 取得浏览器的userAgent字符串
+  if (userAgent.indexOf('OPR') > -1) { // 判断是否Opera浏览器 OPR/43.0.2442.991
+    return 'Opera'
+  } else if (userAgent.indexOf('Firefox') > -1) { // 判断是否Firefox浏览器  Firefox/51.0
+    return 'FF'
+  } else if (userAgent.indexOf('Trident') > -1) { // 判断是否IE浏览器  Trident/7.0; rv:11.0
+    return 'IE'
+  } else if (userAgent.indexOf('Edge') > -1) { // 判断是否Edge浏览器  Edge/14.14393
+    return 'Edge'
+  } else if (userAgent.indexOf('Chrome') > -1) { // Chrome/56.0.2924.87
+    return 'Chrome'
+  } else if (userAgent.indexOf('Safari') > -1) { // 判断是否Safari浏览器
+    return 'Safari'
+  }
+}
+
+export {
+  has,
+  checkBrowser
+}
